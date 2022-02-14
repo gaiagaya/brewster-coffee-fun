2900d921874eb2b4c62e612e6a418d15d5164082
Gaia Corno <lauragaiacorno@gmail.com>
2022-02-14 22:53:17 +0000

    Jo worked some magic on the javascript ... it actually now shows what section is active AND it scrolls v smooth yay ty Jo

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-var navbuttonContainer = document.getElementById("navbar");
-var navbuttons = navbuttonContainer.getElementsByClassName("navbutton");
-for (var i = 0; i < navbuttons.length; i++) {
-    navbuttons[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        if (current.length > 0) {
-            current[0].className = current[0].className.replace (" active","");
-        }
-        this.ClassName += " active";
+const navbuttons = document.querySelectorAll(".navbutton");
+
+navbuttons.forEach(el => {
+    el.addEventListener("click", function(e){
+        setActiveButton(e.target);
     });
+});
+
+function setActiveButton(el) {
+        let active = document.querySelectorAll("[data-active]");
+
+        active.forEach( el => {
+            el.removeAttribute("data-active");
+        });
+
+        el.setAttribute("data-active", true);
 }
+
+let options = {
+    root: document.getElementById("main"),
+    rootMargin: "0px",
+    threshold: 0.3
+}
+
+let setActive = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            let activeSelector = "link-" + entry.target.id;
+            let activeLink = document.getElementById(activeSelector);
+            setActiveButton(activeLink)
+        }
+    })
+}
+
+let observer = new IntersectionObserver(setActive, options)
+
+let observables = document.querySelectorAll(".observable");
+
+observables.forEach(el => {
+    observer.observe(el)
+});
